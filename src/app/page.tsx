da5d4e16836568d8eeb1df8161651ce1f7d9cb47
Bohdan Lobor <4813007+blobor@users.noreply.github.com>
2024-06-11 15:11:27 +0300

    chore(app): add basic component for keygen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,14 @@
+"use client"
+
+import { useEffect, useState } from 'react'
+
+import { generateAESKey } from "../aes"
+
 export default function Page() {
-  return <h1>Hello, Next.js!</h1>
+  const [key, setKey] = useState('')
+  useEffect(() => {
+    generateAESKey().then(setKey, console.error)
+  }, [])
+
+  return <h1>{key}</h1>
 }
